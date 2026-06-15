f7d9a01a29844e43cc2ae670369338f80dd4763e
Martin <mlaut.sdu.dk>
2026-06-15 20:45:55 +0200

    parameter optimization of k, A using Bayesian optimization

---
+++ examples_SDU/multiphase/MRT/csv_plot.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import numpy as np
+
+# === Load CSV ===
+file_path = r"C:\Users\mlaut\GitRepos\JAX-LaB-MicTherm\optuna_all_trials.csv"  
+df = pd.read_csv(file_path)
+# keep only files where T_X = ...
+df = df[df.iloc[:, 0] == 0.45]
+
+# === Choose columns ===
+x_col = df.columns[2]
+y_col = df.columns[3]
+value_col = df.columns[6]
+
+# === Extract where Z is min ===
+idx_min = df[value_col].idxmin()
+
+x_min = df.loc[idx_min, x_col]
+y_min = df.loc[idx_min, y_col]
+z_min = df.loc[idx_min, value_col]
+
+# === Pivot data into grid ===
+pivot_table = df.pivot(index=y_col, columns=x_col, values=value_col)
+
+# === Convert to arrays ===
+X = pivot_table.columns.values
+Y = pivot_table.index.values
+Z = pivot_table.values
+Z = np.ma.masked_invalid(Z)
+
+
+# === Plot heatmap ===
+plt.figure()
+
+sc = plt.scatter(
+    df[x_col],
+    df[y_col],
+    c=df[value_col],
+    cmap="viridis",
+    norm=colors.LogNorm()
+)
+
+plt.scatter(
+    x_min,
+    y_min,
+    color="red",
+    s=100,
+    edgecolor="black",
+    label="Minimum"
+)
+
+
+plt.colorbar(label=value_col)
+plt.xlabel(x_col)
+plt.ylabel(y_col)
+plt.title("Heatmap from CSV")
+
+plt.show()
